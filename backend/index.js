59a5f8585d286e5594ae6d2056490664d6d3a15b
Calvinmuemah <kelvinmuemah855@gmail.com>
2025-03-17 19:42:37 +0300

    modified index.js

--- backend/index.js
+++ backend/index.js
@@ -25,9 +25,10 @@ connectDB();
 const app = express();
 app.use(express.json({ limit: "5mb" })); // Allow large images
 
-// app.use(cors());
+app.use(cors());
 app.use(cors({
-  origin: '*'
+  origin: ['https://bp-track-delta.vercel.app/', 'http://localhost:3000'],
+  methods: ['GET', 'POST',"PUT", "DELETE"],
 }));
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
